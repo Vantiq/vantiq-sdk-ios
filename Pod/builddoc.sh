e822d90c510d112b43a8a48ee1c00c801b5922ab
Michael Swan <swan@feedfriendly.com>
2019-08-26 11:36:26 -0700

    Regenerated html format docs

--- Pod/builddoc.sh
+++ Pod/builddoc.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-appledoc --project-name VIQ --project-company "VANTIQ, Inc." --company-id com.vantiq --keep-intermediate-files --output help .
+appledoc --project-name VIQ --project-company "VANTIQ, Inc." --company-id com.vantiq --keep-intermediate-files --create-html --no-create-docset --no-install-docset --no-publish-docset --output help .
